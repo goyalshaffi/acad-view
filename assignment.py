5085becdff9c7176d5526e922bb38c46f860fa20
goyalshaffi <isha31198@gmail.com>
2018-06-13 20:58:48 +0530

    second assignment completed

---
+++ assignment.py
@@ -0,0 +1,29 @@
+# question 1
+print("hello,i m isha")
+
+#question 2
+a="acad"
+b="view"
+print(a+b)
+
+#question 3
+x=input("enter no")
+y=raw_input("enter value")
+z=input("enter no")
+print(str(x)+y+str(z))
+
+#question4
+print("\"let's get started\"" )
+
+#question 5
+s="acadview"
+course="python"
+fees=5000
+print("%s %s %d"%(s,course,fees))
+
+#question6
+name="tony stark"
+salary=1000000
+print('%s"%d')%(name,salary)
+
+
