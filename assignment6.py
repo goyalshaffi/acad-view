aff4071da9672ddfc67d689fb46875077e6f104c
goyalshaffi <isha31198@gmail.com>
2018-06-20 00:22:33 +0530

    updated

--- assignment6.py
+++ assignment6.py
@@ -1,5 +1,5 @@
 # q1
-'''
+
 for i in range(1,11):
   print(int(input("enter no")))
 
@@ -9,18 +9,83 @@ for i in range(1,11):
 
 while(True):
   print(int(input("enter no")))
-'''
+
 
 
 
 
 # q3
 
+l=[1,5,3,4]
+for i in l:
+  x=i*i
+  print(x)
 
 
 
 
+# q4
+
+l=[1,"sakshi",3.6,2,"uro",5.9,3,"kansal",8.9]
+l1=[]
+l2=[]
+l3=[]
+for i in l:
+  if(isinstance(i,int)):
+    l1.append(i)
+  elif (isinstance(i, str)):
+    l2.append(i)
+  else:
+    l3.append(i)
+print(l3)
+print(l2)
+print(l1)
 
 
 
+# Q5
+
+
+l1=[]
+l2=[]
+for i in range(1,101):
+  if(i%2==0):
+    l1.append(i)
+  else:
+    l2.append(i)
+print(l1)
+print(l2)
+
+
+ #q6
+
+for i in range(0,4):
+  for j in range(0,i+1):
+    print("*",end-" ")
+  print("\r")
+
+
+
+# q7
+
+d1={}
+d3=input("enter no")
+d1.update(d3)
+print(d1)
+for i in d1:
+  print(i)
+
+
+# q8
+
+
+l=[]
+l1=input("enter value")
+l.extend(l1)
+print(l)
+j=input("enter no")
+for i in l:
+  if(i==j):
+    l.remove(i)
+print(l)
 
