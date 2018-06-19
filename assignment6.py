fa8248be07888fc26d92ef3cca1716516c51227b
goyalshaffi <isha31198@gmail.com>
2018-06-19 19:18:02 +0530

    update

--- assignment6.py
+++ assignment6.py
@@ -1,7 +1,25 @@
 # q1
-
+'''
 for i in range(1,11):
-  int(input("enter no")
+  print(int(input("enter no")))
+
+
+# q2
+
+
+while(True):
+  print(int(input("enter no")))
+'''
+
+
+
+
+# q3
+
+
+
+
+
 
 
 
